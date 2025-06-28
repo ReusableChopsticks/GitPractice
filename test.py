a43de41ae432c04da36e87de882a5bac31779906
ReusableChopsticks <76480675+ReusableChopsticks@users.noreply.github.com>
2025-06-28 03:55:46 +0000

    Added comments explaining what I reverted

--- test.py
+++ test.py
@@ -3,3 +3,9 @@ print("Hello, GitHub Codespaces!")
 print("This is a new change. What does git diff show?")
 
 # Making a change from a detached HEAD state by viewing an earlier commit using git checkout <commitID>
+
+# Remember that when you do git revert <commit ID>
+# You revert the changes made in commit ID
+# so it goes back to what the state was before that commit!
+# Also, it is not recommended to git revert a merge from a workflow standpoint
+# https://www.kernel.org/pub/software/scm/git/docs/howto/revert-a-faulty-merge.html
