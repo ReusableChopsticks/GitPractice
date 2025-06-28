4d10c3b842f1061cc5456e43bbc02dde1c5f2626
ReusableChopsticks <76480675+ReusableChopsticks@users.noreply.github.com>
2025-06-28 03:41:47 +0000

    Revert "Adding bad code on purpose to revert later"
    This reverts commit 03c761603b4a313e31ee94066e71c59c3e4d1703.

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 print("Hello, GitHub Codespaces!")
 
 print("This is a new change. What does git diff show?")
-print('this is a bug. Revert me)
 
 # Making a change from a detached HEAD state by viewing an earlier commit using git checkout <commitID>
